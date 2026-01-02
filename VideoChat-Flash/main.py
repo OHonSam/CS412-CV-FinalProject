8483a0ffe09ca455f4d601de9ef71455d7ca4651
lephatminh <22125056@student.hcmus.edu.vn>
2026-01-02 22:33:11 +0700

    Fix

--- VideoChat-Flash/main.py
+++ VideoChat-Flash/main.py
@@ -355,7 +355,7 @@ def main():
     # ============ CONFIGURATION ============
     model_path = "OpenGVLab/VideoChat-Flash-Qwen2-7B_res448"
     video_dir = "/kaggle/input/sutd-traffic-video-qa/SUTD/videos"  # Directory containing videos
-    question_path = "/root/CS412-CV-FinalProject/R2_test.jsonl"  # JSONL file with questions
+    question_path = "/root/CS412-CV-FinalProject/VideoChat-Flash/R2_test.jsonl"  # JSONL file with questions
     output_path = "/root/CS412-CV-FinalProject/results.json"  # Output file for results
     log_interval = 100  # Log every N questions
     # =======================================
